005d6321f30434921218fa1c3169ea262db812e9
Sodman007 <gsodiq123@gmail.com>
2022-12-08 18:59:40 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char fourthTask[] =
+"\"programming is like building a multilingual puzzle";
+
+
+puts(fourthTask);
+
+return (0);
+}
